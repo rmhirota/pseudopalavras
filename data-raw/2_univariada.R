d38ed04f443aeefef86241ad0291ac9c3c563c30
m-almeida <mariana_almeidacosta@usp.br>
2021-05-22 18:07:14 -0300

    atualizações mari

--- data-raw/2_univariada.R
+++ data-raw/2_univariada.R
@@ -27,8 +27,12 @@ dados %>%
 # X-squared = 123.71, df = 6, p-value < 2.2e-16
 
 # CONFIRMAR: número total de palavras em cada grupo é diferente, e o teste usa
-# contagem.
+# contagem. Se temos dois tipos aleatórios (falante e pseudopalavra) esses testes
+# ainda fazem sentido?
 
+tab_grupo = table(dados$grupo,dados$tonicidade_producao)
+CA_grupo=ca(tab_grupo)
+plot(CA_grupo, labels=c(2,2))
 
 # resposta X estrutura ----------------------------------------------------
 
@@ -48,6 +52,9 @@ dados %>%
 # data:  .
 # X-squared = 6120.7, df = 2, p-value < 2.2e-16
 
+tab_est = table(dados$tonicidade_producao,dados$estrutura_palavra)
+CA_est = ca(tab_est)
+plot(CA_est, labels=c(2,2))   #parece que só deu 1 dimensão
 
 # resposta X segmento modificado ------------------------------------------
 
@@ -60,6 +67,7 @@ dados %>%
   dplyr::select(-estrutura_palavra) %>%
   as.matrix() %>%
   chisq.test()
+
 # H_0: estrutura não determina localização acentual (não há associação entre estrutura e tonicidade)
 # H_1: estrutura determina localização acentual (há associação entre estrutura e tonicidade)
 # Pearson's Chi-squared test
@@ -77,14 +85,19 @@ dados %>%
   tidyr::pivot_wider(names_from = tonicidade_producao, values_from = n) %>%
   dplyr::select(-tonicidade_alvo) %>%
   as.matrix() %>%
+
   chisq.test()
 # H_0: tonicidade da palavra-alvo não determina localização acentual
 # (não há associação entre as variáveis)
 # H_1: tonicidade da palavra-alvo determina localização acentual
 # Pearson's Chi-squared test
-#
 # data:  .
 # X-squared = 1823.9, df = 4, p-value < 2.2e-16
+
+
+tab_alvo = table(dados$tonicidade_producao,dados$tonicidade_alvo)
+CA_alvo = ca(tab_alvo)
+plot(CA_alvo, labels=c(2,2))
 
 
 # resposta x validacao ----------------------------------------------------
@@ -102,9 +115,26 @@ dados %>%
   dplyr::filter(grupo == 4, taxa_similaridade == 2) %>%
   dplyr::count(pseudopalavra)
 
+# resposta X taxa de similaridade -----------------------------------------
+
+dados %>%
+  dplyr::mutate(similar = ifelse(
+    taxa_similaridade %in% 1:3, "similar", "dissimilar"
+  ))
+
+dados %>%
+  dplyr::filter(grupo == 4, taxa_similaridade == 2) %>%
+  dplyr::count(pseudopalavra)
 
 
+# resposta x formação --------------------------------------------------------------------
+dados %>% dplyr::distinct(informante, .keep_all = TRUE) %>%
+  dplyr::count(area_formacao,tonicidade_producao) %>%
+  tidyr::pivot_wider(names_from = tonicidade_producao, values_from = n) %>%
+   dplyr::select(-area_formacao) %>% as.matrix() %>%  fisher.test()
 
-
-
+#Fisher's Exact Test for Count Data
+#data:  .
+#p-value = 0.5224
+#alternative hypothesis: two.sided
 
