3bb36a50236d083bb66fc5f688124867b73643f9
m-almeida <mariana_almeidacosta@usp.br>
2021-06-19 18:21:57 -0300

    att modelagem

--- data-raw/modelagem.R
+++ data-raw/modelagem.R
@@ -1,4 +1,4 @@
-#library(mclogit) #unico que funcionou
+library(mclogit) #unico que funcionou
 #library(mlogit)
 #library(lme4)
 #library(mixcat)
@@ -26,7 +26,7 @@ base = pseudopalavras::dados%>%mutate(
                                       linguas = as.factor(linguas))%>%select(
                                         -c(vizinhanca_tonicidade, vizinhanca_fonologica))
 
-
+# n apresentou NA quanto usa proparoxítona de base
 base$tonicidade_producao = relevel(base$tonicidade_producao, ref = "paroxítona")
 
 m = mblogit(tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
@@ -34,11 +34,14 @@ m = mblogit(tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
               aleatorizacao+musica+linguas+genero+escolaridade+idade+
               area_formacao,random = ~1|informante,data=base)
 
+m1 = mblogit(tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+              grupo+segmento_modificado+silaba_modificada+bloco_apresentacao+
+              aleatorizacao+escolaridade,random = ~1|informante,data=base)
 
 
-# random = ~1|informante,
+summary(m1)
 #/eb, subset=classd!="Farmers")
-summary(m)
+
 
 
 #------------------------------------------
@@ -51,10 +54,33 @@ mg = gamlss(formula = tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
               musica+linguas+genero+area_formacao+escolaridade+idade+re(random=~1|informante),
             data=base,control = gamlss.control(n.cyc = 30), family = gamlss.dist::MN3())
 
-
-
 summary(mg)
 getSmo(mg)
+
+mg1 = gamlss(formula = tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+              grupo+segmento_modificado+silaba_modificada+aleatorizacao+bloco_apresentacao+
+              musica+linguas+area_formacao+re(random=~1|informante),
+            data=base,control = gamlss.control(n.cyc = 30), family = gamlss.dist::MN3())
+
+summary(mg1)
+getSmo(mg1)
+
+mg2 = gamlss(formula = tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+               grupo+segmento_modificado+silaba_modificada+aleatorizacao+bloco_apresentacao+
+               musica+linguas+area_formacao+re(random=~1|informante),
+             data=base,control = gamlss.control(n.cyc = 30), family = MULTIN(type = "3"))
+
+summary(mg2)
+getSmo(mg2)
+
+mg3 = gamlss(formula = tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+               grupo+segmento_modificado+silaba_modificada,
+             sigma.formula = tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+               grupo+segmento_modificado+silaba_modificada,
+             data=base,control = gamlss.control(n.cyc = 100), family = MULTIN(type = "3"))
+
+summary(mg3)
+getSmo(mg3)
 
 obj=str(mg)
 
@@ -98,3 +124,36 @@ coef(getSmo(mg)) # fitted coefficients
 intervals(getSmo(mg)) # Confidence intervals
 
 
+#------------------------Apenas palavras validadas
+
+base = pseudopalavras::dados %>%       #10.393
+  filter(validacao == 's'| validacao == 'q' ) %>%
+  mutate(
+  informante=as.factor(informante),
+  tonicidade_producao = as.factor(tonicidade_producao),
+  tonicidade_alvo = as.factor(tonicidade_alvo),
+  estrutura_palavra = as.factor(estrutura_palavra),
+  grupo = as.factor(grupo),
+  musica = as.factor(musica),
+  aleatorizacao = as.factor(aleatorizacao),
+  bloco_apresentacao = as.factor(bloco_apresentacao),
+  vizinhanca_tonicidade = as.factor(vizinhanca_tonicidade),
+  segmento_modificado = as.factor(segmento_modificado),
+  silaba_modificada = as.factor(silaba_modificada),
+  genero = as.factor(genero),
+  escolaridade = as.factor(escolaridade),
+  area_formacao = as.factor(area_formacao),
+  linguas = as.factor(linguas))%>%select(
+    -c(vizinhanca_tonicidade, vizinhanca_fonologica))
+
+base$tonicidade_producao = relevel(base$tonicidade_producao, ref = "paroxítona")
+
+m_val = mblogit(tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+              grupo+segmento_modificado+silaba_modificada+bloco_apresentacao+
+              aleatorizacao+musica+linguas+genero+escolaridade+idade+
+              area_formacao,random = ~1|informante,data=base)
+summary(m_val)
+m_val2 = MASS::stepAIC(mblogit(tonicidade_producao ~ tonicidade_alvo+estrutura_palavra+
+          grupo+segmento_modificado+silaba_modificada+bloco_apresentacao+
+          aleatorizacao+musica+linguas+genero+escolaridade+idade+
+          area_formacao,random = ~1|informante,data=base))
