0d7c375a59d5f2c7fffc05c4f7677413398c0441
Giovanna Vendeiro <giovendeiro@usp.br>
2021-06-29 20:54:22 -0300

    interpretações coefs Modelo 2

--- data-raw/4_modelo_similaridade.R
+++ data-raw/4_modelo_similaridade.R
@@ -14,8 +14,8 @@ base$aleatorizacao = relevel(base$aleatorizacao, ref = "s")
 
 readr::write_rds(base, "data-raw/base_modelo.rds")
 base_sem_naovalidadas <- base %>%
-  dplyr::filter(validacao == "s")
-
+  dplyr::filter(validacao == "s" | validacao == 'q')
+dplyr::glimpse(base_sem_naovalidadas)
 
 # Modelo Completo (com interação)
 mt <- mblogit(
@@ -26,38 +26,37 @@ mt <- mblogit(
   epsilon = 1e-08, maxit = 30, method = "PQL")
 summary(mt)
 
-# Modelo retirando Música - maior p valor nos dois modelos
+# Modelo retirando Linguas - maior p valor nos dois modelos
 m1 <- mblogit(
   tonicidade_producao ~ tonicidade_alvo + estrutura_palavra + similaridade*frequencia +
     segmento_modificado + bloco_apresentacao + aleatorizacao +
-    linguas + idade + genero + escolaridade + area_formacao,
+    musica + idade + genero + escolaridade + area_formacao,
   random = ~1|informante, data = base_sem_naovalidadas,
   epsilon = 1e-08, maxit = 30, method = "PQL")
 summary(m1)
 
-#Modelo retirando Área de formação - maior p valor nos dois modelos
+#Modelo retirando Genero - maior p valor nos dois modelos
 m2 <- mblogit(
   tonicidade_producao ~ tonicidade_alvo + estrutura_palavra + similaridade*frequencia +
     segmento_modificado + bloco_apresentacao + aleatorizacao +
-    linguas + idade + genero + escolaridade,
+    linguas + idade + area_formacao + escolaridade,
   random = ~1|informante, data = base_sem_naovalidadas,
   epsilon = 1e-08, maxit = 30, method = "PQL")
 summary(m2)
 
-# Modelo retirando Gênero - maior p valor nos dois modelos
+# Modelo retirando Linguas - maior p valor nos dois modelos
 m3 <- mblogit(
   tonicidade_producao ~ tonicidade_alvo + estrutura_palavra + similaridade*frequencia +
-    segmento_modificado + bloco_apresentacao + aleatorizacao +
-    linguas + idade + escolaridade,
+    segmento_modificado + bloco_apresentacao + aleatorizacao + idade + area_formacao + escolaridade,
   random = ~1|informante, data = base_sem_naovalidadas,
   epsilon = 1e-08, maxit = 30, method = "PQL")
 summary(m3)
 
-# Modelo retirando Línguas - maior p valor nos dois modelos
+
+# Modelo retirando Area de formação - maior p valor nos dois modelos
 m4 <- mblogit(
   tonicidade_producao ~ tonicidade_alvo + estrutura_palavra + similaridade*frequencia +
-    segmento_modificado + bloco_apresentacao + aleatorizacao +
-    idade + escolaridade,
+    segmento_modificado + bloco_apresentacao + aleatorizacao + idade + escolaridade,
   random = ~1|informante, data = base_sem_naovalidadas,
   epsilon = 1e-08, maxit = 30, method = "PQL")
 summary(m4)
@@ -119,7 +118,7 @@ mclogit:::AIC.mclogit(m9)
 # Praticamente não teve diferença, então vamos mantar o modelo mais simples,
 # sem interação.
 
-# mf = m9
+# mf2 = m9
 mf2 <- mblogit(
   tonicidade_producao ~ tonicidade_alvo + estrutura_palavra + similaridade +
     frequencia + segmento_modificado + bloco_apresentacao + aleatorizacao,
@@ -178,7 +177,57 @@ summary(mf_25)
 
 # Interpretação -----------------------------------------------------------
 
+coef_oxitonas = exp(mf2$coefficients)[c(1,3,5,7,9,11,13,15,17,19,21)]
+
+coef_proparoxitonas = exp(mf2$coefficients)[c(2,4,6,8,10,12,14,16,18,20,22)]
+
+#Oxítonas versus Paroxítonas -----------------------------------------------------------
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando sua palavra alvo é paroxítona é aproximadamente 1/5 (0.247) da probabilidade dela ser acentuada como
+como oxítona (e não como paroxítona) quando sua palavra alvo é oxítona.
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando sua palavra alvo é proparoxítona é 0.13 vezes a probabilidade dela ser acentuada como
+como oxítona (e não como paroxítona) quando sua palavra alvo é oxítona.
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando sua estrutura é no formato CV-CV-CVC é aproximadament 73 vezes a probabilidade de uma pseudopalavra
+ser acentuada como oxítona (e não como paroxítona) quando sua estrutura é no formato CV-CV-CV.
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando sua palavra alvo é de alta frequência é 0.66 vezes a probabilidade de uma pseudopalavra
+ser acentuada como oxítona (e não como paroxítona) quando sua sua palavra alvo é de baixa frequência.
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando a mudança ocorreu em uma vogal é 1.21 vezes a probabilidade de uma pseudopalavra
+ser acentuada como oxítona (e não como paroxítona) quando a mudança ocorreu em uma consoante.
+
+A probabilidade de uma pseudopalavra ser acentuada como oxítona (e não como paroxítona)
+quando não houve aleatorização é aproximadamente 0.53 vezes a probabilidade de uma pseudopalavra
+ser acentuada como oxítona (e não como paroxítona) quando houve aleatorização.
 
 
+#Proparoxítonas versus Paroxítonas -----------------------------------------------------------
 
+
+A probabilidade de uma pseudopalavra ser acentuada como proparoxítona (e não como paroxítona)
+quando sua palavra alvo é paroxítona é aproximadamente 0.54 da probabilidade dela ser acentuada como
+como proparoxítona (e não como paroxítona) quando sua palavra alvo é oxítona.
+
+A probabilidade de uma pseudopalavra ser acentuada como proparoxítona (e não como paroxítona)
+quando sua palavra alvo é proparoxítona é 5.27 vezes a probabilidade dela ser acentuada como
+como proparoxítona (e não como paroxítona) quando sua palavra alvo é oxítona.
+
+A probabilidade de uma pseudopalavra ser acentuada como proparoxítona (e não como paroxítona)
+quando sua estrutura é no formato CV-CV-CVC é 6.10 vezes a probabilidade de uma pseudopalavra
+ser acentuada como proparoxítona (e não como paroxítona) quando sua estrutura é no formato CV-CV-CV.
+
+A probabilidade de uma pseudopalavra ser acentuada como proparoxítona (e não como paroxítona)
+quando ela foi classificada como similar à sua palavra alvo é 3.7 vezes a probabilidade de uma pseudopalavra
+ser acentuada como proparoxítona (e não como paroxítona) quando ela foi classificada como dissimilar à sua palavra alvo.
+
+A probabilidade de uma pseudopalavra ser acentuada como proparoxítona (e não como paroxítona)
+quando a mudança ocorreu em uma vogal é 1.34 vezes a probabilidade de uma pseudopalavra
+ser acentuada como proparoxítona (e não como paroxítona) quando a mudança ocorreu em uma consoante.
 
